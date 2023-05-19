60b4c5b9655e025b0979340e4eb7126659dcb4cd
deltw32 <rosoftmike199@gmail.com>
2023-05-19 22:40:08 +0800

    hw5

--- CSIE4004_hw5/nothing_bad/test.py
+++ CSIE4004_hw5/nothing_bad/test.py
@@ -1,14 +1,19 @@
 from pwn import *
 import time
+myfile=open("out.txt",mode='w+')
 def connect_to_c2_agent( cmdId, data ):
     io = remote("127.0.0.1", 7373)
     header = b'nslab_w31c0m3_U\x00'
     cmd = cmdId + b'\x00' * ( 4 - len(cmdId) )
     content = header + cmd + data
     io.send(content)
-    # whatever you want.
+    # whatever you want
+    try:
+        response=io.recvline()
+        myfile.write(f"cmdId:{cmdId}, response:{response}\n")
+    except:
+        io.close()
     io.close()
-for i in range(0000,9999):
-    string=str(i)
-    id=string.encode()
-    connect_to_c2_agent(id,b'b10902035');
+for i in range(10000):
+    cmdId=str(i).zfill(4).encode()
+    connect_to_c2_agent(cmdId,b'b10902035')
